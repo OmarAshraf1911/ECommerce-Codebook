cc5a75fa18afbb5fb5105157120f371de89712c2
OmarAshraf1911 <omarashraf.90.5.370@gmail.com>
2024-08-20 22:38:43 +0300

    Create produstCard

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,9 +1,15 @@
-import React from "react";
+import { Hero } from "./components/Hero";
+import { FeaturedProducts } from "./components/FeaturedProducts";
+import { Testimonials } from "./components/Testimonials";
+import { Faq } from "./components/Faq";
 
 export const HomePage = () => {
   return (
     <main>
-      <div>HomePage</div>
+      <Hero />
+      <FeaturedProducts />
+      <Testimonials />
+      <Faq />
     </main>
   );
 };
